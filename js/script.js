2d63b1891313be7a8813a237d3b3b3f41a13ae3d
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-05-26 14:22:44 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let genderFemaleSum = null;
+let countGenderFemaleSum = 0;
+
+let genderMaleSum = null;
+let cauntGenderMaleSum = 0;
+
+let ageSum = null;
+let cauntAgeSum = 0;
+
+let averageAges = null;
+
+window.addEventListener('load', function () {
+  doFetch();
+  usersFound();
+  statistics();
+});
+
+async function doFetch() {
+  const response = await fetch(
+    'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
+  );
+  const dataResponse = await response.json();
+  console.log(dataResponse);
+}
+
+function usersFound() {
+  quantityUsersFound();
+  quantityRenderUsers();
+}
+
+function statistics() {
+  quantityGenderFemale();
+  quantityGenderMale();
+  quantityAge();
+  averageAge();
+}
+
+function quantityUsersFound() {}
+
+function quantityRenderUsers() {}
+
+function quantityGenderFemale() {}
+
+function quantityGenderMale() {}
+
+function quantityAge() {}
+
+function averageAge() {}
